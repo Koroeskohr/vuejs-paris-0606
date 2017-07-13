454f79ed5fc68f081f405e022ee137edcb71b786
Victor Viale <somebodywas@gmail.com>
2017-07-13 22:55:42 +0200

    Create a test scene for entity component

--- test5/src/components/Renderer.vue
+++ test5/src/components/Renderer.vue
@@ -9,13 +9,15 @@
   import Assets from 'components/Assets'
   import SceneTest from 'components/scenes/SceneTest'
   import SceneTestTwo from 'components/scenes/SceneTestTwo'
+  import EntityTest from 'components/scenes/EntityTest'
 
   export default {
     name: "Renderer",
     components: {
       "assets": Assets,
       "scene-test": SceneTest,
-      "scene-test-two": SceneTestTwo
+      "scene-test-two": SceneTestTwo,
+      "entity-test": EntityTest,
     },
     computed: {
       currentScene() {
---
+++ test5/src/components/scenes/EntityTest.vue
@@ -0,0 +1,22 @@
+<template>
+  <a-entity>
+    <Entity test="test" azeraze="azerazer">
+      <Entity geometry="primitive: box;" material='color: blue;'></entity>
+      <Entity geometry="primitive: box;" material='color: red;'></entity>
+    </Entity>
+  </a-entity>
+</template>
+
+<script>
+import Entity from "../Entity"
+
+  export default {
+    name: "EntityTest",
+    components: {
+      Entity
+    },
+    data() {
+      return {}
+    }
+  }
+</script>
